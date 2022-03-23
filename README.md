ceb0a233abec1e7aab088fd400cd5caf7ef8ecfe
xfangfang <2553041586@qq.com>
2022-03-23 21:24:27 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # homebrew-Macast
+
+This repo contains custom mpv and ffmpeg homebrew tap for Macast.
+
+## Main
+
+### Casks/macast-dev.rb
+
+Install Macast.app (dev version)
+
+```
+brew tap xfangfang/Macast
+brew install macast-dev
+```
+
+If you want to install the official version, please run: `brew install macast`
+
+### mpv-macast.rb
+
+Depends on ffmpeg-macast instead of ffmpeg
+Does not depend on vapoursynth and yt-dlp
+
+
+### ffmpeg-macast.rb
+
+Removed all encoding libraries
+Removed some of the decoding libraries which ffmpeg can natively decode
+Patch for some HLS video (For personal use, the release version of Macast.app does not contain this modification)
+
+## Others
+
+### build mpv for Macast.app
+
+If you want to build macast locally App may refer to the following content
+
+##### step 1: build Macast.app (without mpv)
+
+You can refer to the construction steps in the [Macast wiki](https://github.com/xfangfang/Macast/wiki/BuildFromSource)
+
+On the Mac of Intel chip, we can directly copy the officially compiled mpv into the Macast.app, but the official does not provide the mpv binary file of Apple silicon chip, so we need to compile mpv manually.
+
+##### step 2: build mpv and copy to Macast.app
+
+```
+# build mpv
+
+cd Macast
+brew tap xfangfang/Macast
+brew install mpv-macast
+```
+
+Find where mpv located: `which mpv` and make sure this mpv is the one you just built.
+
+Then copy mpv to Macast.app.
+
+```
+mkdir -p dist/Macast.app/Contents/Resources/bin/MacOS/
+cp `which mpv` dist/Macast.app/Contents/Resources/bin/MacOS/mpv
+```
+
+Finally, copy the libraries which mpv dependent into Macast.app
+
+```
+# brew install dylibbundler
+dylibbundler -od -b \
+-x dist/Macast.app/Contents/Resources/bin/MacOS/mpv \
+-d dist/Macast.app/Contents/Resources/bin/MacOS/lib/ \
+-p @executable_path/lib/
+
+codesign --sign - --force dist/Macast.app/Contents/Resources/bin/MacOS/lib/*
+
+```
+
+## Links
+
+https://github.com/xfangfang/Macast/blob/main/.github/workflows/build-macast.yaml
+
+https://github.com/xfangfang/Macast/wiki/BuildFromSource
+
+https://github.com/xfangfang/Macast/issues/11
+
+https://github.com/xfangfang/Macast/releases/tag/v0.1
---
+++ ffmpeg-macast.rb
@@ -0,0 +1,89 @@
+# modified from https://github.com/iina/homebrew-mpv-iina
+# Last check with upstream: 5ef3900b6178dee40629e3e058a587ef196b53b1
+# https://github.com/Homebrew/homebrew-core/blob/master/Formula/ffmpeg.rb
+
+class FfmpegMacast < Formula
+  desc "Play, record, convert, and stream audio and video"
+  homepage "https://ffmpeg.org/"
+  url "https://github.com/xfangfang/FFmpeg/archive/patch/hls_4.4.zip"
+  sha256 "7bfff0c1e2179892f8b735961599daf35e2bffa26ee99dd9a29765a5a157b5f9"
+  head "https://github.com/xfangfang/FFmpeg.git", branch: "patch/hls_4.4"
+
+  depends_on "nasm" => :build
+  depends_on "pkg-config" => :build
+  depends_on "dav1d"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "frei0r"
+  depends_on "gnutls"
+  depends_on "libass"
+  depends_on "libbluray"
+  depends_on "libsoxr"
+  depends_on "libvidstab"
+  depends_on "rubberband"
+  depends_on "snappy"
+  depends_on "speex"
+  depends_on "tesseract"
+  depends_on "xz"
+  depends_on "zeromq"
+  depends_on "zimg"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  def install
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-gnutls
+      --enable-gpl
+      --enable-libbluray
+      --enable-libdav1d
+      --enable-librubberband
+      --enable-libsnappy
+      --enable-libtesseract
+      --enable-libvidstab
+      --enable-libxml2
+      --enable-libfontconfig
+      --enable-libfreetype
+      --enable-frei0r
+      --enable-libass
+      --enable-libspeex
+      --enable-libsoxr
+      --enable-libzmq
+      --enable-libzimg
+      --disable-libjack
+      --disable-indev=jack
+      --disable-programs
+    ]
+
+      # Needs corefoundation, coremedia, corevideo
+     args << "--enable-videotoolbox" if OS.mac?
+     args << "--enable-neon" if Hardware::CPU.arm?
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install Dir["tools/*"].select { |f| File.executable? f }
+
+    # Fix for Non-executables that were installed to bin/
+    mv bin/"python", pkgshare/"python", force: true
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_predicate mp4out, :exist?
+  end
+end
